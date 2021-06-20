1434642c7bf244edcb8adcbb95364f546e7335bd
Hebzi <dumaradek@gmail.com>
2021-06-20 10:46:03 +0200

    Create, Update and Delete Posts
    - Now logged user can add new posts
    - Users are able to update and delete their posts

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,18 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    datePosted = models.DateTimeField(default=timezone.now)
+    date_posted = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        # return reverse('blog-home') <- redirects to home page
+        return reverse('post-detail', kwargs={'pk': self.pk})  # redirects to post details
